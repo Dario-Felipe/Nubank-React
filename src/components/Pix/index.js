468c04b2c7684568e29fdefef4b45e7744a1eeac
Dario-Felipe <dariofelipe91@gmail.com>
2020-12-29 16:59:31 -0300

    First commit

---
+++ src/components/Pix/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+
+const Pix = () => {
+  return (
+    <div className="section-pix">
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
+
+export default Pix
+
