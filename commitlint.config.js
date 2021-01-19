ca12d29072777d56ba3f4538635a91213f8bffd8
Dario-Felipe <dariofelipe91@gmail.com>
2021-01-19 11:40:30 -0300

    feat: add commitlint, eslint and prettier

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'ci',
+        'chore',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
+};
